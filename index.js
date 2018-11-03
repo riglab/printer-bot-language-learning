5f740e3d5dc9a7cf5ba6340b09481f5bf27142a5
egendreau <egendreau254@gmail.com>
2018-11-03 10:17:34 -0400

    ground work for text-to-speech

--- index.js
+++ index.js
@@ -20,6 +20,41 @@ app.get('/voice-recognizer', (req, res) => {
   //res.render('test', { stuffFromServer: messageFile, pid: participantId });
 });
 
+// Imports the Google Cloud client library
+const textToSpeech = require('@google-cloud/text-to-speech');
+
+// Creates a client
+const client = new textToSpeech.TextToSpeechClient();
+
+// // The text to synthesize
+// const text = 'Hi, I\'m Pingu';
+// 
+// // Construct the request
+// const request = {
+//   input: {text: text},
+//   // Select the language and SSML Voice Gender (optional)
+//   voice: {languageCode: 'en-US', 'name':'en-US-Wavenet-D', ssmlGender: 'MALE'},
+//   // Select the type of audio encoding
+//   audioConfig: {audioEncoding: 'MP3'},
+// };
+
+// Performs the Text-to-Speech request
+client.synthesizeSpeech(request, (err, response) => {
+  if (err) {
+    console.error('ERROR:', err);
+    return;
+  }
+
+  // Write the binary audio content to a local file
+  fs.writeFile('audio/output.mp3', response.audioContent, 'binary', err => {
+    if (err) {
+      console.error('ERROR:', err);
+      return;
+    }
+    console.log('Audio content written to file: output.mp3');
+  });
+});
+
 app.get('/writeTxt', (req, res) => {
   req.setTimeout(0)
   const participantId = req.query.id;
